9d1e575be041240caef416e8ba01766600cfb917
Paulyne Wambui <paulynewambui2020@gmail.com>
2026-03-10 15:44:23 +0300

    add the my work section

---
+++ src/assets/mywork_data.js
@@ -0,0 +1,42 @@
+import glasses from "../assets/glasses.jpg"
+ import stalks from "../assets/stalks.jpg"
+ import leave from "../assets/leave.jpg"
+import code from "../assets/code.jpg"
+ import colors from "../assets/colors.jpg"
+import book from "../assets/book.jpg"
+
+const mywork_data = [
+
+    {
+        w_no:1,
+        w_name: "Web design",
+        w_img:glasses
+    },
+     {
+        w_no:2,
+        w_name: "Playstation",
+        w_img:stalks
+     },
+{
+         w_no:3,
+         w_name: "Nextjs",
+         w_img:leave
+     },
+ {
+            w_no:4,
+         w_name: "Portfolio",
+         w_img:code
+     },
+      {
+        w_no:5,
+         w_name: "Service Tracker",
+         w_img:colors
+        },
+    {
+        w_no:6,
+        w_name: "Graphql",
+        w_img:book
+    }
+]
+
+export default mywork_data;
